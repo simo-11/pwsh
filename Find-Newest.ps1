3b7359dadf2612440aa2da650c51b0179c2425f1
Simo Nikula <simo.nikula@gmail.com>
2024-01-16 19:06:07 +0200

    Find-Newest.ps1 added

---
+++ Find-Newest.ps1
@@ -0,0 +1,15 @@
+﻿#
+param (
+    [string]$path = "."
+ )
+$newest=@{
+FullName=""
+LastWriteTime=(Get-Date -Date "1970-01-01 00:00:00Z")
+}
+Get-ChildItem -Path $path -Recurse | ForEach-Object {
+    if($_.LastWriteTime -gt $newest.LastWriteTime){
+        $newest.LastWriteTime=$_.LastWriteTime
+        $newest.FullName=$_.FullName
+        Write-Host $newest.FullName $newest.LastWriteTime
+    }
+}
